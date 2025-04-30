55773b4c98afc37d26145618412b60e85ebcb367
LuixzSouza <luiz.antoniodesouza004@gmail.com>
2025-04-29 22:10:19 -0300

    Criando react

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className='div1' >
+          <h1>Luz Antonio de Souza</h1>
+          <p>Sou um Dev.Jr Front-End atualmente trabalho como Freelancer e estou a procura de uma oportunidade</p>
+        </div>
+        <div className='hob hob1' >
+          <p>HOB 01</p>
+        </div>
+        <div className='hob hob2'>
+          <p>HOB 02</p>
+        </div>
+        <div className='hob hob3'>
+          <p>HOB 03</p>
+        </div>
+        <h2 className='texto-vermelho' >Skill</h2>
+        <ul>
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>Node</li>
+        </ul>
+    </>
+  )
+}
+
+export default App
